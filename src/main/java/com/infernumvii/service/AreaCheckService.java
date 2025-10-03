142abfd9ac965fda4e27eaa7aadff094f2814710
InfenrumVII <egor554shved@gmail.com>
2025-10-03 17:51:07 +0300

    some improv

--- src/main/java/com/infernumvii/service/AreaCheckService.java
+++ src/main/java/com/infernumvii/service/AreaCheckService.java
@@ -8,26 +8,28 @@ import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class AreaCheckService {
-
     public boolean isPointInTheArea(Cords cords){
         BigDecimal x = cords.getX();
         double y = cords.getY();
         double R = cords.getR();
-        //TODO p = x.pow(2).add(new BigDecimal(y).pow(2))
-        //    .compareTo(new BigDecimal(R * R / 4.0f)) <= 0 
-        
+
+        boolean exactCircleCond = x.pow(2).add(new BigDecimal(y).pow(2))
+                                    .compareTo(new BigDecimal(R * R / 4.0f)) <= 0;                             
         boolean circleCond = (
             x.compareTo(BigDecimal.ZERO) >= 0 &&
-            y <= 0 &&
-            x.pow(2).add(new BigDecimal(y).pow(2))
-                .compareTo(new BigDecimal(R * R / 4.0f)) <= 0
+            y <= 0 && exactCircleCond
+            
         );
+        
+        boolean exactTriangleCond = new BigDecimal(y)
+                                        .compareTo(x.add(new BigDecimal(R/2.0f))) <= 0;
         boolean triangleCond = (
                 x.compareTo(BigDecimal.ZERO) <= 0 &&
                 y >= 0 &&
-                y <= R/2.0f &&
-                new BigDecimal(y).compareTo(x.add(new BigDecimal(R/2.0f))) <= 0
+                y <= R/2.0f && exactTriangleCond
+                
         );
+
         boolean squareCond = (
             x.compareTo(BigDecimal.ZERO) >= 0 &&
             y >= 0 &&
